e7fccfb0e96345e5b1b71fd9dac935afe30f8b1b
Oren <orengolan@gmail.com>
2015-08-16 18:59:48 -0700

    cities

--- index.js
+++ index.js
@@ -1,31 +1,80 @@
+'use strict';
+
 require('./env.js');
-var cypher = require("./cypher.js");
+var cypher = require('./cypher.js');
 
 var params = [];
 var query = '';
 
-query = `
-  CREATE CONSTRAINT ON (t:Traveler)
-  ASSERT t.email IS UNIQUE;
-`;
-cypher(query, params, done)
+// createCities();
+createAirPorts();
+createTravelers();
+createFlights();
 
-query = `CREATE
-  (t1:Traveler {name: 'Josh'}),
-  (t2:Traveler {name: 'Matt'})
-`;
-cypher(query, params, done);
+function createCities() {
+  query = `CREATE (c:City { props })`;
+  params = [
+    {name: 'Atlanta'},
+    {name: 'Chicago'},
+    {name: 'Los Angeles'},
+    {name: 'Dallas-Fort Worth'},
+    {name: 'Denver'},
+    {name: 'New York'},
+    {name: 'San Francisco'}
+  ];
+  cypher(query, params, done);
+}
 
-query = `CREATE (t:Traveler {name: 'Meg'})`;
-cypher(query, params, done);
+// CREATE (n)-[:LOVES {since: {value}}]->(m)
+// ATL ORD LAX DFW DEN JFK SFO LGA
 
-query = `CREATE (t:Traveler { props })`;
-param = [
-  {name:"Josh"},
-  {name:"Matt"},
-  {name:"Ari"}
-];
-cypher(query, param, done);
+// CREATE (le:Person {name:"Euler"}),(db:Person {name:"Bernoulli"}),
+//   (le)-[:KNOWS {since:1768}]->(db)
+// RETURN le, db
+function createAirPorts() {
+  query = `
+    CREATE (a:Airport {code: 'ATL'}), (c:City {name: 'Atlanta'}),
+    (a)-[:IN]->(c)
+  `
+  // params = [
+  //   {code: 'ATL'},
+  // ];
+  cypher(query, params, done);
+}
+
+function createTravelers() {
+  query = `
+    CREATE CONSTRAINT ON (t:Traveler)
+    ASSERT t.email IS UNIQUE;
+  `;
+  cypher(query, params, done);
+
+  query = `CREATE
+    (t1:Traveler {name: 'Josh'}),
+    (t2:Traveler {name: 'Matt'})
+  `;
+  cypher(query, params, done);
+
+  query = `CREATE (t:Traveler {name: 'Meg'})`;
+  cypher(query, params, done);
+
+  query = `CREATE (t:Traveler { props })`;
+  params = [
+    {name: 'Josh'},
+    {name: 'Matt'},
+    {name: 'Ari'}
+  ];
+  cypher(query, params, done);
+}
+
+function createFlights() {
+  query = `CREATE (f:Flight { props })`;
+  params = [
+    {number: 'LA-232', from: 'LA', to: 'London', date: new Date()},
+    {number: 'NY-843',from: 'NY', to: 'Paris', date: new Date()}
+  ];
+  cypher(query, params, done);
+}
 
 function done(err,data) {
   if (err) {
@@ -33,5 +82,5 @@ function done(err,data) {
     return;
   }
 
-  console.log(JSON.stringify(data))
+  console.log(JSON.stringify(data));
 }
